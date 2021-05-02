a412f16d1f159f542adc4c160a75b2ce2c4f1221
HexagonSun <HexagonSun@users.noreply.github.com>
2021-05-02 13:25:15 +0200

    Fix (new) lint errors

--- src/app/util/enumUtils.ts
+++ src/app/util/enumUtils.ts
@@ -1,12 +1,12 @@
 class EnumUtils {
 
-    public static readonly size = <T extends {}>(enumDefinition: T): number => {
+    public static readonly size = <T extends Record<string, unknown>>(enumDefinition: T): number => {
         const half: number = 2;
 
         return Object.keys(enumDefinition).length / half;
     }
 
-    public static readonly getRandom = <T extends {}>(enumDefinition: T): number =>
+    public static readonly getRandom = <T extends Record<string, unknown>>(enumDefinition: T): number =>
         Math.floor(Math.random() * EnumUtils.size(enumDefinition))
 
 }
